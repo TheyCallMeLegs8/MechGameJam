2446c2fde26f232a7a9b1206214976d1e114009b
BawbFishman <brettwalker2004@gmail.com>
2026-03-13 17:33:51 -0700

    imported rock texture and made rock shader

---
+++ Assets/Scripts/PauseMenu_Brett.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PauseMenu_Brett : MonoBehaviour
+{
+    // People in youtube tutorials will make a variable public
+    // this is cringe unless they are purposefully accessing it from another script
+    // otherwise use [SerializeField]
+    // Jam a man of fortune
+    [SerializeField] private GameObject brett;
+}
